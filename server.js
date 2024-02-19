16f4b18c6ae6deb31b6376684cad4b16080e8a30
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2024-02-19 01:12:37 +0100

    chaning server.js

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ dotenv.config();
 
 const { DB_HOST: uriDb } = process.env;
 
+const PORT = 3001;
+
 const connection = mongsoose.connect(uriDb);
 
 export const startServer = async () => {
   try {
     await connection;
     console.log("Database connection successful");
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   } catch (err) {
     console.log("db not connected");
